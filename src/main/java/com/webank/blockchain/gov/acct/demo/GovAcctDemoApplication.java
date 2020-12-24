db97166b435dee6ecdbf7a8b2154573d6ffe458b
dalaocu <njumjy06@126.com>
2020-12-24 22:29:27 +0800

    init

---
+++ src/main/java/com/webank/blockchain/gov/acct/demo/GovAcctDemoApplication.java
@@ -0,0 +1,13 @@
+package com.webank.blockchain.gov.acct.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovAcctDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovAcctDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/webank/blockchain/gov/acct/demo/config/GovAcctDemoBeanConfig.java
@@ -0,0 +1,41 @@
+package com.webank.blockchain.gov.acct.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.webank.blockchain.gov.acct.manager.AdminModeGovernManager;
+import com.webank.blockchain.gov.acct.manager.EndUserOperManager;
+import com.webank.blockchain.gov.acct.manager.GovernAccountInitializer;
+import com.webank.blockchain.gov.acct.manager.SocialVoteManager;
+import com.webank.blockchain.gov.acct.manager.VoteModeGovernManager;
+
+@Configuration
+public class GovAcctDemoBeanConfig {
+
+    @Bean
+    public GovernAccountInitializer getGovernAccountInitializer() {
+        return new GovernAccountInitializer();
+    }
+    
+    @Bean
+    public EndUserOperManager getEndUserOperManager() {
+        return new EndUserOperManager();
+    }
+
+    @Bean
+    public SocialVoteManager getSocialVoteManager() {
+        return new SocialVoteManager();
+    }
+
+    @Bean
+    public AdminModeGovernManager getAdminModeGovernManager() {
+        return new AdminModeGovernManager();
+    }
+    
+    @Bean
+    public VoteModeGovernManager getVoteModeGovernManager() {
+        return new VoteModeGovernManager();
+    }
+    
+
+}
